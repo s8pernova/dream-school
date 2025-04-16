8e3713d7a4ad9d1e28d245abc6e252d014257b7b
Maha <maha.ahmed4peace@gmail.com>
2025-04-16 13:24:26 -0400

    update: adding basic JSON transfer data questions and transfer form questionairre skeleton

--- dream-school/src/App.jsx
+++ dream-school/src/App.jsx
@@ -1,15 +1,19 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import TransferForm from "./pages/TransferForm.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
 function App() {
 	return (
 		<>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
+			
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/transform" element={<TransferForm />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			
 		</>
 	);
 }
--- dream-school/src/pages/Home.jsx
+++ dream-school/src/pages/Home.jsx
@@ -1,11 +1,21 @@
+import React from 'react'; 
+import {useNavigate} from 'react-router-dom'; 
+
 const Home = () => {
+
+	const navigate = useNavigate(); 
+
+	const handleStartClick = () => {
+		navigate("/transform"); //navigate to Transfer Form
+	}
+
 	return (
 		<>
 			<div className="home-container">
 				<span className="welcome-text">
 					Find your path from NOVA to your dream school!
 				</span>
-				<button className="start-button">Get Started</button>
+				<button className="start-button" onClick={handleStartClick}>Get Started</button>
 			</div>
 		</>
 	);
